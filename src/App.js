c81516984da4e68ddbd408ea515b43024aedd76d
santiagoOTF <santiago.gutierrez@onthefuze.com.au>
2024-02-14 23:20:00 -0500

    logo4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.png';
+import logoEspiral from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoEspiral} className="App-logo" alt="logo" />
         <p>
           Hola! Somos Espiral Raiz
         </p>
