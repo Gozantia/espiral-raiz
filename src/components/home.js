cd67206968c66e53c86045fcd365a35463f4bb0f
gozantia <lordzantiago@gmail.com>
2024-02-28 16:20:30 -0500

    video

--- src/components/home.js
+++ src/components/home.js
@@ -6,9 +6,18 @@ import React from 'react';
 
 const Home = () => {
     return ( 
-        <div>
-            <h1>Home Espiral Raiz</h1>
-        </div>
+        <div> 
+            <div className="wrapper-video">
+            <div className='overlay-bg'> layer </div>
+            <div className='video-container'></div>
+            <iframe className='hero-video' width="560" height="315" src="https://www.youtube.com/embed/FSbPdl_LntU?si=XlcZTHLn5i9O5ezO&amp;controls=0&amp;start=102&amp;autoplay=1&amp;mute=1&amp;playsinline=1&amp;loop=1&amp;showinfo=0" title='Home video'></iframe>
+            </div>
+            <div className='hero-text'>
+                 <h1>Home Espiral Raiz</h1>
+            </div>
+            <div>
+            </div>
+              </div>
 
      );
 }
